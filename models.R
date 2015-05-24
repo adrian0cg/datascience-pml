bac3e2b11d1777518cb22a2bec7ad743f1dace0a
Adrian Hass <adrian.hass@gmail.com>
2015-05-24 09:35:13 +0200

    next try

--- models.R
+++ models.R
@@ -1,3 +1,15 @@
-plmTrainControl <- trainControl(method="LOOCV", p=0.9, number=10, classProbs=TRUE)
+plmTrainControl <- trainControl(
+    method="LOOCV", p=0.9,
+    number=4, classProbs=TRUE,
+    verboseIter=TRUE
+)
+
+adaFit <- train(
+    classe ~., data=pmlTrain[inValidation[1],],
+    preProc=c("center","scale"),
+    method="ada",
+    trControl=plmTrainControl,
+    metric="ROC"
+)
 
 svmFit <- train(classe ~., data=pmlTrain, method="svmPoly",preProc=c("center","scale"),tuneLength=8, metric="ROC")
